4a6c83b384cca9e0c8f034479a50e0d4299208ef
Michael Gruper <sarge43210@gmail.com>
2024-09-25 07:27:11 -0500

    Initial Commit

---
+++ ClickerGameStoreGruperTests/ClickerGameStoreGruperTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClickerGameStoreGruperTests.swift
+//  ClickerGameStoreGruperTests
+//
+//  Created by MICHAEL GRUPER on 9/25/24.
+//
+
+import Testing
+@testable import ClickerGameStoreGruper
+
+struct ClickerGameStoreGruperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
